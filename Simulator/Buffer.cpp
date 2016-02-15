16d32cab0b00b6f670aa2347e74092a7a9cdadc9
Vladislav Boboshko <vboboshko@stc-spb.ru>
2016-02-15 22:02:29 +0300

    Artur's buffer changes

--- Simulator/Buffer.cpp
+++ Simulator/Buffer.cpp
@@ -1,73 +1,62 @@
 #include "stdafx.h"
 #include "Buffer.h"
 
-Buffer::Buffer(int size) {
-	this->size = size;
+Buffer::Buffer(int size):
+	m_size(size)
+{
+	this->m_size = size;
 	this->count = 0;
+	buff = QVector<Customer*>(size, NULL);
 }
 
 Buffer::~Buffer() {}
 
 Customer* Buffer::push(Customer* customer) {
-	if (buff.size() == size) {
-		uint min(0xFFFFFFFF), quantity(0);
-		int index(-1);
-		double maxTime(0);
-		for (int i=0; i<buff.size(); i++) {
-			if (buff[i]->getSourceNo() < min)
-				min = buff[i]->getSourceNo();
-		}
-		for (int i=0; i<buff.size(); i++) {
-			if ((buff[i]->getSourceNo() == min) && (buff[i]->getArrivalTime() > maxTime)) {
-				maxTime = buff[i]->getArrivalTime();
-				index = i;
-			}
-		}
-		if (customer->getSourceNo() < buff[index]->getSourceNo()) {
-			std::swap(customer, buff[index]);
-			return customer;
-		if ((customer->getArrivalTime() <= buff[index]->getArrivalTime())
-				&& customer->getSourceNo() == buff[index]->getSourceNo()) {
-				std::swap(customer, buff[index]);
-				return customer;
-			}
-		}
-		return customer;
-	}
-	buff.append(customer);
 	count++;
-	return NULL;
+	for (int i=0; i < this->m_size; i++)
+	{
+		if (!buff[i]) {
+			buff[i] = customer;
+			return NULL;
+		}
+	}
+	return customer;
 }
 
 Customer* Buffer::pop() {
-	if (buff.isEmpty()) {
+	if (this->isEmpty()) {
 		return NULL;
 	}
-
-	unsigned short min(0xFFFF), index(0);
-	int i;
-	for (i=0; i<buff.size(); i++) {
-		if (buff[i]->getSourceNo() < min) {
-			min = buff[i]->getSourceNo();
-			index = i;
+	uint minSourceNo(0xFFFFFFFF);
+	double minTime(INFINITY);
+	uint res(0xFFFFFFFF);
+	for (uint i=0; i < this->m_size; i++) {
+		if (buff[i] && buff[i]->getSourceNo() < minSourceNo) minSourceNo = buff[i]->getSourceNo();
+	}
+	for (uint i=0; i < this->m_size; i++) {
+		if (buff[i] && buff[i]->getArrivalTime() < minTime) {
+			minTime = buff[i]->getArrivalTime();
+			res = i;
 		}
 	}
-
-	Customer* res = new Customer(buff.at(index)->getSourceNo(),buff.at(index)->getNo(), buff.at(index)->getArrivalTime());
-	buff.remove(index);
-	count--;
-	return res;
+	Customer* result = new Customer(buff[res]->getSourceNo(), buff[res]->getNo(), buff[res]->getArrivalTime());
+	delete buff[res];
+	buff[res]=NULL;
+	return result;
 }
 
 
 bool Buffer::isEmpty()
 {
-	return buff.isEmpty();
+	foreach (auto i, buff) {
+		if (i) return false;
+	}
+	return true;
 }
 
 void Buffer::print()
 {
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < m_size; i++)
 	{
 		if (buff[i] != NULL)
 		{
@@ -82,22 +71,19 @@ void Buffer::print()
 
 vector<string> Buffer::getBuffer()
 {
-	vector<string> res(size, "");
-	for (int i = 0; i < size; i++)
+	vector<string> res(m_size, "");
+	for (int i = 0; i < m_size; i++)
 	{
-		if (i<count)
-		{
-			res[i] += "Customer: " + to_string(buff[i]->getSourceNo()) + "-"+to_string(buff[i]->getNo());
-		}
-		else
-		{
-			res[i] += "-----";
+		if (buff[i]) {
+			if (i<count)
+			{
+				res[i] += "Customer: " + to_string(buff[i]->getSourceNo()) + "-"+to_string(buff[i]->getNo());
+			}
+			else
+			{
+				res[i] += "-----";
+			}
 		}
 	}
 	return res;
 }
-
-
-
-
-
